15dc5172e537f78abd433f7ce20aff67ab1848f3
ERR0RW0LF <finn.woma@gmail.com>
2026-08-08 02:35:09 +0200

    added checks for winner

--- cogs/tic_tac_toe.py
+++ cogs/tic_tac_toe.py
@@ -5,6 +5,40 @@ import logging
 logger = logging.getLogger(__name__)
 
 SIZE_OF_BOARD = 3
+WIN_PATTERNS = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0]
+]
+
+num_to_reaction = {
+    0: ":one:",
+    1: ":two:",
+    2: ":three:",
+    3: ":four:",
+    4: ":five:",
+    5: ":six:",
+    6: ":seven:",
+    7: ":eight:",
+    8: ":nine:"
+}
+
+reaction_to_num = {
+    ":one:": 0,
+    ":two:": 1,
+    ":three:": 2,
+    ":four:": 3,
+    ":five:": 4,
+    ":six:": 5,
+    ":seven:": 6,
+    ":eight:": 7,
+    ":nine:": 8
+}
 
 class Tic_tac_toe(Cog):
     def __init__(self, bot: fluxer.Bot):
@@ -28,6 +62,39 @@ class Tic_tac_toe(Cog):
         )
         
         game_message = await ctx.reply(embed=game_embed)
+        
+        game_finished = False
+        
+        while not(game_finished):
+            for num in game_board:
+                await game_message.add_reaction(num_to_reaction[num])
+            break
+    
+    
+    def pattern_check(self, board: list) -> tuple[bool, str]:
+        has_winner = False
+        winner = None
+        possible_winner = None
+        
+        for pattern in WIN_PATTERNS:
+            for cell_n in range(0,len(pattern)):
+                pattern_cell = pattern[cell_n]
+                cell = board[cell_n]
+                if not(possible_winner) and pattern_cell == 1:
+                    if str(cell).isdigit():
+                        break
+                    possible_winner = str(cell)
+                elif pattern_cell == 1:
+                    if str(cell) != possible_winner:
+                        winner = None
+                        break
+                    winner = cell
+            if winner: 
+                break
+        
+        
+        
+        return (has_winner, winner)
     
     def pretty_board(self, board: list) -> str:
         board_string = ""
